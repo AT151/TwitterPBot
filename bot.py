201d773cbc063214766f96cfde2874be4db3e3b0
AT151 <anh.t.tran151@gmail.com>
2022-02-17 16:19:22 -0500

    added text to deep fried image

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ def add_noise(image):
     sigma = var**0.5
     gauss = np.random.normal(mean,sigma,(row,col,ch))
     gauss = gauss.reshape(row,col,ch)
-    noisy = img + img + gauss
-    
+    noisy = img * 2 + gauss
+
     # normalize image to range [0,255]
     minv = np.amin(noisy)
     maxv = np.amax(noisy)
@@ -22,32 +22,54 @@ def add_noise(image):
 
     return Image.fromarray(noisy)
 
+def add_text(image):
+    x, y = image.size[0] / 5, image.size[1] / 4
+
+    text = "Bottom Text"
+    draw = ImageDraw.Draw(image)
+    font_type = ImageFont.truetype("Arial.ttf", 128)  # 18 -> font size
+    # thin border
+    draw.text((x-1, y), text, font=font_type, fill="black")
+    draw.text((x+1, y), text, font=font_type, fill="black")
+    draw.text((x, y-1), text, font=font_type, fill="black")
+    draw.text((x, y+1), text, font=font_type, fill="black")
+
+    # thicker border
+    draw.text((x-1, y-1), text, font=font_type, fill="black")
+    draw.text((x+1, y-1), text, font=font_type, fill="black")
+    draw.text((x-1, y+1), text, font=font_type, fill="black")
+    draw.text((x+1, y+1), text, font=font_type, fill="black")
+
+    draw.text((x, y), text, (255, 255, 255), font=font_type) 
+
 def recieve_image():
     reddit = praw.Reddit(client_id = config.client_id,
             client_secret = config.client_secret,
             user_agent="user agent")
 
-    subreddit = reddit.subreddit("nsfw")
+    subreddit = reddit.subreddit("cats")
 
-    hot_posts = subreddit.hot(limit = 5)
+    hot_posts = subreddit.top(limit = 5)
 
     for post in hot_posts:
-        if ".jpg" in post.url:
-            with open('pic.jpg', 'wb') as f:
+        if ".jpg" in post.url or ".png" in post.url:
+            with open('pic.png', 'wb') as f:
                 f.write(requests.get(post.url).content)
 
 def deep_fry_image():
-    image = Image.open('pic.jpg')
+    image = Image.open('pic.png')
 
-    
+
+    add_text(image)
     image = add_noise(image)
     image = ImageEnhance.Color(image).enhance(3)
-    image = ImageEnhance.Contrast(image).enhance(3)
+    image = ImageEnhance.Contrast(image).enhance(5)
     image = ImageEnhance.Brightness(image).enhance(1.5)
-    image = ImageEnhance.Sharpness(image).enhance(3)
-
     
-    image.save("ced.jpg")
+    image = ImageEnhance.Sharpness(image).enhance(3)
+    
+
+    image.save("fried.png")
 
 
 def main():
